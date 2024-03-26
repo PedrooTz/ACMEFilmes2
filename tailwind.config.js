d512b7838bee6a0c844b14f10230d161eda4b911
Pedro Henrique <pedrohenrique1603@gmail.com>
2024-03-26 15:25:03 -0300

    Em processo

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      background: {
+        'rrr': "url('./img/background.webp')"
+      }
+    },
+  },
+  plugins: [],
+}
+
