1fdff077c6b1e6fd576f74b36c97b3149b4c45b7
Pedro Henrique <pedrohenrique1603@gmail.com>
2024-04-02 17:14:07 -0300

    Segunda tela finalizada e cadastro iniciado

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        topo: '#000000',
+        baixo: '#BB0000'
+      },
       backgroundImage: {
         'back': "url('./img/background.jpg')"
       }
